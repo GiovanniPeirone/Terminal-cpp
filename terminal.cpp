3208b38bfe87777eed177d31844537abf42049a2
Giovanni Peirone <mgpeirone25@gmail.com>
2025-02-22 18:28:35 -0300

    teminal

--- terminal.cpp
+++ terminal.cpp
@@ -1,9 +1,32 @@
+#include <cstdlib>
 #include <iostream>
+#include <windows.h>
 
 using namespace std;
 
 int main() {
+   
+    while (true) {
 
-        cout << ;
+
+        char buffer[MAX_PATH];
+
+        if (GetCurrentDirectoryA(MAX_PATH, buffer)){
+            cout << "f" << buffer << ">"; 
+        }
+
+        else {
+            cout << "error" << endl;
+        }
+       
+
+        char *Command;
+
+        cin >> Command;
+
+        system(Command);
+
+    }
+    
 }
 
